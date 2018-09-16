9b97505696054a28a23d2dfbfbe26c94fe1f87ed
Leila Aparecida da Silva <leilaapsilva26@gmail.com>
2018-09-16 17:24:42 -0300

    Nome do arquivo agora é definido no cliente
    Adiciona comentários ao código

--- redes/servidor.py
+++ redes/servidor.py
@@ -1,50 +1,47 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
-import socket
 
-print("servidor iniciado, aguardando cliente")
+"""
+servidor.py
+Autora: Leila Aparecida da Silva
+Etapa 1 do trabalho da disciplina de Redes de Computadores
+Servidor TCP que cria um arquivo .txt com nome e conteúdo enviados pelo cliente.
+"""
+
+import socket 
 
 host = ''
 port = 8000
 
+print "Servidor iniciado na porta", port
+print "Aguardando cliente..."
+
 #conexao entre dois ips
 #sock_stream - tcp
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria um socket TCP/IP
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 s.bind((host, port)) #aguarda uma conexao
-s.listen(1) #pode lidar com ate 1 conexao ao mesmo tempo (tentar mudar isso depois)
+s.listen(1) #pode lidar com ate 1 conexao ao mesmo tempo 
 
 cli, addr = s.accept() #recebe uma conexao e a aceita
-arq = open('teste.txt', 'w') #abre em modo de escrita arquivo que vai receber as informacoes
+
+print"\nCliente com IP ", addr[0], "conectado!"
+
+name = cli.recv(100) #recebe até 100 bytes do cliente
+
+name = name + '.txt' #concatena a extensão do arquivo
+
+arq = open(name, 'w') #cria o arquivo em modo de escrita
 
 while 1:
-	dados = cli.recv(1024)
+	dados = cli.recv(1024) #recebe o texto a ser escrito no arquivo
 	if not dados:
 		break
  	arq.write(dados)
 
-arq.close()
-cli.close()
+print "\nArquivo ", name, " criado com o texto enviado pelo cliente ;) ", dados 
 
+arq.close() #fecha o arquivo
+cli.close() #encerra a conexão 
 
-
-
-
-
-"""
-while True:
-    cli, addr = s.accept() #recebe uma conexao e a aceita
-
-    print('Servidor conectado por ', addr)
-
-    dados = b''
-    contador = 0
-    while not b'F' in dados:   # experimente não colocar este while
-        dados += cli.recv(1000000)
-        contador += 1
-    print('Recebi %d bytes' % len(dados))
-  #  cli.send('recebido pelo servidor' + dados)
-
-print('Executei recv() %d vezes' % contador)
-
-"""
